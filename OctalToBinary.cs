5ab851f7040275e403df2969d6080c2e9360cc7e
Ivy Chang <ivycwq@icloud.com>
2016-09-10 15:44:44 -0700

    fixing bugs

--- OctalToBinary.cs
+++ OctalToBinary.cs
@@ -4,7 +4,7 @@ namespace BaseConverterRefactor {
   public class OctalToBinary : BaseConverterRefactor {
     OctalToDecimal octalToDecimal = new OctalToDecimal();
     public override void PrintResult() {
-      Console.WriteLine("{0} in {1} is {2} in {3}. ", Number, FromBase, Convert.ToString(octalToDecimal.GetResult(), 8), ToBase);
+      Console.WriteLine("{0} in {1} is {2} in {3}. ", Number, FromBase, Convert.ToString(octalToDecimal.GetResult(), 2), ToBase);
     }
   }
 }
