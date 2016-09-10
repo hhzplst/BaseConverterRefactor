0267acbdbb239deb767c0f3829158bda358e5b32
Ivy Chang <ivycwq@icloud.com>
2016-09-10 15:40:12 -0700

    adding result getters

--- HexToDecimal.cs
+++ HexToDecimal.cs
@@ -2,8 +2,11 @@ using System;
 
 namespace BaseConverterRefactor {
   public class HexToDecimal : BaseConverterRefactor {
+    public int GetResult() {
+      return Convert.ToInt32(Convert.ToString(Number), 16);
+    }
     public override void PrintResult() {
-      Console.WriteLine("{0} in {1} is {2} in {3}. ", Number, FromBase, Convert.ToInt32(Convert.ToString(Number), 16), ToBase);
+      Console.WriteLine("{0} in {1} is {2} in {3}. ", Number, FromBase, GetResult(), ToBase);
     }
   }
 }
--- OctalToDecimal.cs
+++ OctalToDecimal.cs
@@ -2,8 +2,11 @@ using System;
 
 namespace BaseConverterRefactor {
   public class OctalToDecimal : BaseConverterRefactor {
+    public int GetResult() {
+      return Convert.ToInt32(Convert.ToString(Number), 8);
+    }
     public override void PrintResult() {
-      Console.WriteLine("{0} in {1} is {2} in {3}. ", Number, FromBase, Convert.ToInt32(Convert.ToString(Number), 8), ToBase);
+      Console.WriteLine("{0} in {1} is {2} in {3}. ", Number, FromBase, GetResult(), ToBase);
     }
   }
 }
